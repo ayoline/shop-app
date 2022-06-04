ed6aa283f0a165f46027a49c708a6f459cfba007
ayoline <ayo@outlook.com.br>
2022-06-04 13:51:04 -0300

    undoing 'add to cart' actions

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -62,6 +62,19 @@ class ProductItem extends StatelessWidget {
             icon: const Icon(Icons.shopping_cart),
             onPressed: () {
               cart.addItem(product.id, product.price, product.title);
+              Scaffold.of(context).hideCurrentSnackBar();
+              Scaffold.of(context).showSnackBar(
+                SnackBar(
+                  content: const Text('Added item to Cart!'),
+                  duration: const Duration(seconds: 2),
+                  action: SnackBarAction(
+                    label: 'UNDO',
+                    onPressed: () {
+                      cart.removeSingleItem(product.id);
+                    },
+                  ),
+                ),
+              );
             },
             color: Theme.of(context).accentColor,
           ),
