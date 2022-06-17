ede86ca8724e7a5f8e80ed8fc041cfe416e4eeae
ayoline <ayo@outlook.com.br>
2022-06-17 13:38:42 -0300

    validate user input

--- lib/screens/edit_product_screen.dart
+++ lib/screens/edit_product_screen.dart
@@ -48,11 +48,9 @@ class _EditProductScreenState extends State<EditProductScreen> {
   }
 
   void _saveForm() {
+    final isValid = _form.currentState!.validate();
+    if (!isValid) {}
     _form.currentState!.save();
-    print(_editedProduct.description);
-    print(_editedProduct.price);
-    print(_editedProduct.imageUrl);
-    print(_editedProduct.title);
   }
 
   @override
@@ -78,6 +76,12 @@ class _EditProductScreenState extends State<EditProductScreen> {
                 textInputAction: TextInputAction.next,
                 onFieldSubmitted: (_) {
                   FocusScope.of(context).requestFocus(_priceFocusNode);
+                },
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return 'Please provide a value!';
+                  }
+                  return null;
                 },
                 onSaved: (value) {
                   _editedProduct = Product(
